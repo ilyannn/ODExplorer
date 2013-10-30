f7359c4ca7fc7890381f5671e83e6dfaf58cb560
ilya <ilya.nikokoshev@gmail.com>
2013-10-30 18:10:37 +0400

    minor - don’t edit rows

--- OIDataNorth/ODResourceViewController.m
+++ OIDataNorth/ODResourceViewController.m
@@ -70,6 +70,10 @@ NSString *const ODCollectionCellReuseID = @"CollectionCell";
     [self.refreshControl endRefreshing];
 }
 
+- (void)refreshChildren {
+    [self.resource retrieve];
+}
+
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     
@@ -129,7 +133,7 @@ NSString *const ODCollectionCellReuseID = @"CollectionCell";
 
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     // Return NO if you do not want the specified item to be editable.
-    return YES;
+    return NO;
 }
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
