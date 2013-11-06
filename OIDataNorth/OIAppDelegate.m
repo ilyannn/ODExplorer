2a3a7d856e78f2b4220fd3184d999fbcf4401fc5
ilya <ilya.nikokoshev@gmail.com>
2013-11-07 02:20:48 +0400

    comment out test URL

--- OIDataNorth/OIAppDelegate.m
+++ OIDataNorth/OIAppDelegate.m
@@ -9,59 +9,51 @@
 #import "OIAppDelegate.h"
 
 #import "ODFavoritesViewController.h"
-#import "ODEntity.h"
+#import "ODResource.h"
 
 @implementation OIAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     ODFavoritesViewController *fav = [ODFavoritesViewController new];
     self.window.rootViewController = fav;
     
-    NSURL *launchURL = [NSURL URLWithString:@"odata://services.odata.org/V3/OData/OData.svc/"];
-    if (launchURL || (launchURL = launchOptions[UIApplicationLaunchOptionsURLKey])) {
+    NSURL *launchURL; // = [NSURL URLWithString:@"odata://services.odata.org/V3/OData/OData.svc/"];
+    if ((launchURL = launchOptions[UIApplicationLaunchOptionsURLKey])) {
         if (![launchURL.scheme hasPrefix:@"http"]) {
             launchURL = [[NSURL alloc] initWithScheme:@"http" host:[launchURL host] path:[launchURL path]];
         }
         [fav pushResource:[ODResource resourceWithURL:launchURL description:@"launch parameter"]];
     }
-
+    
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
+- (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
+- (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
+- (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
-{
+- (void)applicationWillTerminate:(UIApplication *)application {
     // Saves changes in the application's managed object context before the application terminates.
-
 }
 
 #pragma mark - Application's Documents directory
 
 // Returns the URL to the application's Documents directory.
-- (NSURL *)applicationDocumentsDirectory
-{
+- (NSURL *)applicationDocumentsDirectory {
     return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
 }
 
