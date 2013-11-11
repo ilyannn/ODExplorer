89c5367430be91753d50e3e6599f7fcdbad6aa25
ilya <ilya.nikokoshev@gmail.com>
2013-11-11 17:51:07 +0400

    files added

---
+++ Examples/ODFavorites.m
@@ -0,0 +1,75 @@
+//
+//  ODFavorites.m
+//  OIDataNorth
+//
+//  Created by ilya on 11/6/13.
+//  Copyright (c) 2013 Ilya Nikokoshev. All rights reserved.
+//
+
+#import "ODFavorites.h"
+
+#import "ODRetrieving_Objects.h"
+#import "ODCollection.h"
+
+#import "NorthwindService.h"
+
+@implementation ODFavorites
+
++ (instancetype)sharedFavorites {
+    static ODFavorites *shared;
+    if (!shared) {
+        shared = [[self alloc] initFromDefaults];
+    }
+    return shared;
+}
+
+- (instancetype)initFromDefaults {
+    self = [self initWithRetrievalInfo:[ODRetrieveBase new]];
+    if (self) {
+        self.kind = ODResourceKindCollection;
+        [self loadFromDefaults];
+    }
+    return self;
+}
+
+- (void)saveToDefaults {
+    
+}
+
+- (void)loadFromDefaults {
+    ODRetrieveByURL *info = [ODRetrieveByURL new];
+    info.URL = [NSURL URLWithString:@"http://services.odata.org/V3/OData/OData.svc/"];
+    info.shortDescription = @"OData Example";
+    //    info.parent = self.retrievalInfo;
+    
+    
+    ODCollection *exampleService = [ODCollection resourceWithInfo:info];
+    
+    self.childrenArray = [NSMutableArray arrayWithObjects:exampleService,
+                          
+                          [NorthwindService unique],
+                          
+                          [ODCollection resourceWithDict: @{
+                                                            @"uri":  @"http://packages.nuget.org/v1/FeedService.svc/Packages",
+                                                            @"name": @"NuGet Packages"
+                                                            }],
+                          
+                          [ODCollection resourceWithDict:   @{
+                                                              @"uri":  @"http://tv.telerik.com/services/odata.svc/",
+                                                              @"name": @"Telerik"
+                                                              }],
+                          
+                          [ODCollection resourceWithDict: @{  @"uri":  @"http://odata4j-sample.appspot.com/datastore.svc/",
+                                                              @"name": @"OData4j Sample"
+                                                              }],
+                          
+                          nil];
+    
+}
+
+- (NSString *)shortDescription {
+    return NSLocalizedString(@"Favorites", @"Description of favorites list");
+}
+
+
+@end
